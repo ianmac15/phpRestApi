a9296a9500e8fa8d9ffbd250b4d8583e1b442d8a
ianmac15 <3thita@gmail.com>
2022-08-08 22:50:22 +0300

    1

--- src/controllers/read.php
+++ src/controllers/read.php
@@ -15,8 +15,36 @@ $product = new Product($db);
 $productServices = new ProductServices($product);
 $result = $productServices->getAll();
 
-if (isset($result)) {
-    echo json_encode($result);
-} else {
-    echo json_encode(array('message' => 'There are no products'));
+// if (isset($result)) {
+//     echo json_encode($result);
+// } else {
+//     echo json_encode(array('message' => 'There are no products'));
+// }
+
+$xml = new DOMDocument('1.0');
+$xml->formatOutput = true;
+$products = $xml->createElement("products");
+$xml->appendChild($products);
+
+for ($i = 0; $i < count($result); $i++) {
+    $product = $xml->createElement("product");
+    $products->appendChild($product);
+
+    $id = $xml->createElement("id", $result[$i]['id']);
+    $product->appendChild($id);
+
+    $pname = $xml->createElement("name", $result[$i]['pname']);
+    $product->appendChild($pname);
+
+    $category = $xml->createElement("category", $result[$i]['category']);
+    $product->appendChild($category);
+
+    $price = $xml->createElement("price", $result[$i]['price']);
+    $product->appendChild($price);  
+
+    $created_at = $xml->createElement("created_at", $result[$i]['created_at']);
+    $product->appendChild($created_at);
 }
+
+echo "" . $xml->saveXML() . "";
+$xml->save("report.xml");
